739154e801546cdd0eed5a30e5e5f4f013220701
Alberto <alberto_fernandezolalla@hotmail.com>
2014-11-20 12:04:44 +0100

    botones guais

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions'},path_names: { sign_in: 'login', sign_up: 'signup' }
   get 'home' => 'home#welcome'
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'home#welcome'
+  root 'home#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
