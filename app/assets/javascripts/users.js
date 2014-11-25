b048513a381c7fc162e2b5a3837693a8211d3d19
Alberto <alberto_fernandezolalla@hotmail.com>
2014-11-25 17:49:59 +0100

    fotosmap

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,26 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function showlocation(){
+
+
+var madrid = new google.maps.LatLng(40.4430018,-3.5847271);
+var opciones = {
+    zoom : 5,
+    center: madrid,
+ 
+};
+var div = document.getElementById('mapa');
+var map = new google.maps.Map(div, opciones);
+
+
+putMarker(41.3956445,2.2064281);
+putMarker(38.925229,1.4831543);
+putMarker(40.4430018,-3.5847271);
+
+function putMarker(lat, lon){
+	return new google.maps.Marker({
+  position: new google.maps.LatLng(lat,lon),
+  map: map
+});
+}
+};	
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
           has_many :casas
+  # existing code
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  do_not_validate_attachment_file_type :avatar
 end
