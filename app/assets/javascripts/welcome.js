3d6eb374f6afbf6852cd9ff76b4acc5390248f46
Alberto <alberto_fernandezolalla@hotmail.com>
2014-11-26 10:43:53 +0100

    mapa

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -22,27 +22,3 @@ function avanzaSlide(){
     contador = (contador + 1) % 5;
     mostrar(arrayImagenes[contador]);
 }
-function showlocation(){
-
-
-var madrid = new google.maps.LatLng(40.4430018,-3.5847271);
-var opciones = {
-    zoom : 5,
-    center: madrid,
- 
-};
-var div = document.getElementById('mapa');
-var map = new google.maps.Map(div, opciones);
-
-
-putMarker(41.3956445,2.2064281);
-putMarker(38.925229,1.4831543);
-putMarker(40.4430018,-3.5847271);
-
-function putMarker(lat, lon){
-	return new google.maps.Marker({
-  position: new google.maps.LatLng(lat,lon),
-  map: map
-});
-}
-};	
